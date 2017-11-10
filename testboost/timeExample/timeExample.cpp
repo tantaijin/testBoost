fbdcbe94dcc5a081f8b62c60c94a5017579e37bb
taijin.tan <2642058010@qq.com>
2017-11-10 23:10:27 +0800

    time Example modify

--- testboost/timeExample/timeExample.cpp
+++ testboost/timeExample/timeExample.cpp
@@ -3,6 +3,7 @@
 #include "boost\date_time.hpp"
 
 using namespace boost::posix_time;
+using namespace boost::gregorian;
 
 void testTime()
 {
@@ -29,4 +30,15 @@ void testTime()
 	time_duration::tick_type my_millisec = time_duration::ticks_per_second() / 1000; // 1ms
 	time_duration td5(1, 2, 3, 10 * my_millisec); // 10ms
 	cout << "tick: " << td5 << endl;
+
+	ptime pt(date(2017, 11, 10), hours(1));
+	cout << pt << endl;
+	ptime p1 = second_clock::local_time(); // local time
+	ptime p2 = microsec_clock::universal_time(); // utc time
+	cout << p1 << endl << p2 << endl;
+
+	ptime p3(not_a_date_time);
+	assert(p3.is_not_a_date_time());
+	ptime p4(pos_infin); // or neg_infin
+	assert(p4.is_special() && p4.is_infinity());
 }
