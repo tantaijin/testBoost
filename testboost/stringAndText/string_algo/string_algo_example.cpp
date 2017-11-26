a66a30286023dd27884ab509b80019f783e8eb4f
taijin.tan <2642058010@qq.com>
2017-11-26 17:20:26 +0800

    string_algo  join

--- testboost/stringAndText/string_algo/string_algo_example.cpp
+++ testboost/stringAndText/string_algo/string_algo_example.cpp
@@ -1,6 +1,9 @@
 #include "stdafx.h"
 #include "string_algo_example.h"
+#include "boost/format.hpp"
+#include "boost/typeof/typeof.hpp"
 #include "boost/algorithm/string.hpp"
+#include "boost/assign.hpp"
 
 void test_string_algo()
 {
@@ -41,24 +44,102 @@ void test_string_algo()
 	std::string	str2("hello");
 	assert(!boost::is_equal()(str1, str2));
 
+	/*
+	** trim
+	*/
 	std::string strFmt(" hello world! ");
-	cout << boost::trim_copy(strFmt) << endl;
-	cout << boost::trim_left_copy(strFmt) << endl;
-	cout << boost::trim_right_copy(strFmt) << endl;
-	/*
-	hello world!
-	hello world!
-	hello world!
-	*/
+	cout << boost::trim_copy(strFmt) << endl; // hello world!
+	cout << boost::trim_left_copy(strFmt) << endl; // hello world!
+	cout << boost::trim_right_copy(strFmt) << endl; // hello world!
+
 	std::string strFmt2("2017  hello world  123!!!");
-	cout << boost::trim_copy_if(strFmt2, boost::is_digit()) << endl;
-	cout << boost::trim_left_copy_if(strFmt2, boost::is_digit()) << endl;
-	cout << boost::trim_right_copy_if(strFmt2, boost::is_punct()) << endl;
-	cout << boost::trim_copy_if(strFmt2, boost::is_punct() || boost::is_digit()) << endl;
+	cout << boost::trim_copy_if(strFmt2, boost::is_digit()) << endl; // hello world  123!!!
+	cout << boost::trim_left_copy_if(strFmt2, boost::is_digit()) << endl; // hello world  123!!!
+	cout << boost::trim_right_copy_if(strFmt2, boost::is_punct()) << endl; // 2017  hello world  123
+	cout << boost::trim_copy_if(strFmt2, boost::is_punct() || boost::is_digit()) << endl; // hello world
+
 	/*
-	hello world  123!!!
-	hello world  123!!!
-	2017  hello world  123
-	hello world
+	** find
 	*/
+	boost::format fmt("|%s|.pos = %d\n");
+	std::string strrge("Long long ago, there was a king.");
+	boost::iterator_range<std::string::iterator> rge;
+	rge = boost::find_first(strrge, "long");
+	cout << fmt % rge % (rge.begin() - strrge.begin()); // |long|.pos = 5
+	rge = boost::ifind_first(strrge, "long");
+	cout << fmt % rge % (rge.begin() - strrge.begin()); // |Long|.pos = 0
+	rge = boost::find_nth(strrge, "ng", 2); // 3th
+	cout << fmt % rge % (rge.begin() - strrge.begin()); // |ng|.pos = 29
+	rge = boost::find_head(strrge, 4);
+	cout << fmt % rge % (rge.begin() - strrge.begin()); // |Long|.pos = 0
+	rge = boost::find_tail(strrge, 5);
+	cout << fmt % rge % (rge.begin() - strrge.begin()); // |king.|.pos = 27
+	rge = boost::find_tail(strrge, 5);
+	cout << fmt % rge % (rge.begin() - strrge.begin()); // |king.|.pos = 27
+	rge = boost::find_first(strrge, "tantaijn");
+	assert(rge.empty() && !rge);
+
+	/*
+	** replace and erase, like find
+	*/
+	//boost::replace_first boost::replace_first_copy
+	//boost::erase_head boost::erase_head_copy
+
+	/*
+	** ifind_all  split
+	*/
+	std::string strFindAll("Samus,Link.Zelda::Mario-Luii+zelda");
+	std::deque<std::string> d;
+	boost::ifind_all(d, strFindAll, "ZELda");
+	for (BOOST_AUTO(pos, d.begin()); pos != d.end(); ++pos)
+	{
+		cout << "[" << *pos << "]" << endl;
+	}
+
+	std::list<boost::iterator_range<std::string::iterator>> l;
+	boost::split(l, strFindAll, boost::is_any_of(",.:-+"));
+	for (BOOST_AUTO(pos, l.begin()); pos != l.end(); ++pos)
+	{
+		cout << "[" << *pos << "]";
+	}
+	cout << endl;
+
+	l.clear();
+	boost::split(l, strFindAll, boost::is_any_of(",.:-+"), boost::token_compress_on); // token_compress_on, ingore two char same together
+	for (BOOST_AUTO(pos, l.begin()); pos != l.end(); ++pos)
+	{
+		cout << "[" << *pos << "]";
+	}
+	cout << endl;
+
+	/*
+	** join join_if
+	*/
+	std::vector<std::string> v3 = boost::assign::list_of("Samus")("Link")("Zelda")("Mario")("Luii")("zelda");
+	cout << boost::join(v3, "+") << endl;
+	struct is_contains_a{
+		bool operator()(const std::string& x){
+			return boost::contains(x, "a");
+		}
+	};
+	cout << boost::join_if(v3, "+", is_contains_a()) << endl;
+
+	/*
+	** boost::find_iterator boost::split_iterator
+	*/
+	std::string str3("Samus,Link,Zelda,Mario-Luii+zelda");
+	typedef boost::find_iterator<std::string::iterator> string_find_iter;
+	string_find_iter spos, send;
+	for (spos = boost::make_find_iterator(str3, boost::first_finder("Samus", boost::is_equal())); spos != send; ++spos )
+	{
+		cout << "[" << *spos << "]";
+	}
+	cout << endl;
+	typedef boost::split_iterator<std::string::iterator> string_split_iter;
+	string_split_iter sppos, spend;
+	for (sppos = boost::make_split_iterator(str3, boost::first_finder(",", boost::is_equal())); sppos != spend; ++sppos)
+	{
+		cout << "[" << *sppos << "]";
+	}
+	cout << endl;
 }
