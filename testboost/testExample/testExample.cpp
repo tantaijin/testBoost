66997dbed0ed97ed16f29dbebbb27774a5c4da01
taijin.tan <2642058010@qq.com>
2017-11-30 22:50:43 +0800

    BOOST_STATIC_ASSERT

--- testboost/testExample/testExample.cpp
+++ testboost/testExample/testExample.cpp
@@ -5,6 +5,7 @@
 #include "boost/format.hpp"
 
 namespace boost{
+	// can be effective in release
 	void assertion_failed(char const * expr, char const * function, char const * file, long line){
 		boost::format fmt("user defined failed info:\n %s\n%s\n%s\n%d\n");
 		fmt %expr%function%file%line;
@@ -14,10 +15,13 @@ namespace boost{
 
 void fun(int x)
 {
-	BOOST_ASSERT(x != 0 && "error drive by 0");
+	BOOST_ASSERT(x != 0 && "error drive by 0"); // only effective in debug
+
+	//BOOST_STATIC_ASSERT(sizeof(int) == 1); // error C2338: sizeof(int) == 1
 }
 
 void testTest()
 {
 	fun(0);
+	// test lib, need to learn
 }
