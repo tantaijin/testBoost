7197211bea004ee106dd08f7a67c6375eb7a2c7d
taijin.tan <2642058010@qq.com>
2017-12-12 23:27:45 +0800

    boost::tie(j, boost::tuples::ignore) = tup_func();

--- testboost/ContainerExample/tuple_example/tuple_example.cpp
+++ testboost/ContainerExample/tuple_example/tuple_example.cpp
@@ -3,6 +3,18 @@
 #include "boost/typeof/typeof.hpp"
 #include "boost/assign.hpp"
 #include "boost/tuple/tuple.hpp"
+#include "boost/tuple/tuple_io.hpp"
+
+boost::tuple<int, std::string> tup_func(){
+	return boost::make_tuple(1, "hello world");
+}
+
+template <typename T>
+void print_tuple(const T& t){
+	cout << t.get_head() << ",";
+	print_tuple(t.get_tail());
+}
+void print_tuple(const boost::tuples::null_type& t){}
 
 void test_tuple_example()
 {
@@ -12,4 +24,18 @@ void test_tuple_example()
 	cout << tp.get<0>() << endl; // 1
 	cout << tp.get<1>() << endl; // 100
 	cout << boost::get<0>(tp) << endl; // 1
+
+	typedef boost::tuple<int, double, std::string> idsType;
+	idsType tp2 = boost::make_tuple(1, 2.0, "hello world");
+	cout << tp2 << endl; // (1 2 hello world)
+	cout << boost::tuples::set_open('[') << boost::tuples::set_close(']') << boost::tuples::set_delimiter(',');
+	cout << tp2 << endl; // [1,2,hello world]
+
+	int i; std::string str;
+	boost::tie(i, str) = tup_func();
+	cout << i << ", " << str << endl; // 1, hello world
+	int j;
+	boost::tie(j, boost::tuples::ignore) = tup_func();
+
+	print_tuple(tp2); // 1,2,hello world,
 }
