6bb346dcd636facb9f0f3f354ddbbbdd8d61e02c
taijin.tan <2642058010@qq.com>
2017-12-21 22:38:34 +0800

    assert(v2.type() == typeid(int));

--- testboost/ContainerExample/variant_example/variant_example.cpp
+++ testboost/ContainerExample/variant_example/variant_example.cpp
@@ -7,4 +7,13 @@ void test_variant_example()
 	boost::variant<int, float, std::string> v;
 	v = "123";
 	cout << boost::get<std::string>(v) << endl;
+
+	typedef boost::variant<int, double, std::string> var_t;
+	var_t v2;
+	assert(v2.type() == typeid(int));
+	assert(v2.which() == 0);
+
+	v2 = "variant string";
+	cout << boost::get<std::string>(v2) << endl; // variant string
+	cout << *boost::get<std::string>(&v2) << endl; // variant string
 }
