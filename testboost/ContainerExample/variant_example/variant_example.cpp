b417a9fa1b9e8b0c522000618b6eda6882273331
taijin.tan <2642058010@qq.com>
2017-12-26 23:27:56 +0800

    boost::apply_visitor(vp, v3)

--- testboost/ContainerExample/variant_example/variant_example.cpp
+++ testboost/ContainerExample/variant_example/variant_example.cpp
@@ -2,6 +2,14 @@
 #include "variant_example.h"
 #include "boost/variant.hpp"
 
+struct var_print :public boost::static_visitor < > {
+	template <typename T>
+	void operator()(T &i) const{
+		i *= 2;
+		cout << i << endl;
+	}
+};
+
 void test_variant_example()
 {
 	boost::variant<int, float, std::string> v;
@@ -16,4 +24,12 @@ void test_variant_example()
 	v2 = "variant string";
 	cout << boost::get<std::string>(v2) << endl; // variant string
 	cout << *boost::get<std::string>(&v2) << endl; // variant string
+
+	typedef boost::variant<int, double> var_t2;
+	var_t2 v3;
+	v3 = 1;
+	var_print vp;
+	boost::apply_visitor(vp, v3); // 2
+	v3 = 2.3;
+	boost::apply_visitor(vp, v3); // 4.6
 }
